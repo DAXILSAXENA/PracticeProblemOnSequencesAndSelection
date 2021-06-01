cf6eb6285faf2e68d1f5b612448d1d4220da101b
DAXILSAXENA <daxildaxil@gmail.com>
2021-06-01 18:41:18 +0530

    Read a single digit no. and write the number

---
+++ ReadAndWriteNumberCase.sh
@@ -0,0 +1,19 @@
+#! /bin/bash -x
+
+# Read a single digit number and write the number in word using Case
+
+read -p "Enter the number 0 to 9:" number
+
+case $number in
+        0) echo "zero";;
+        1) echo "one";;
+        2) echo "two";;
+        3) echo "three";;
+        4) echo "four";;
+        5) echo "five";;
+        6) echo "six";;
+        7) echo "seven";;
+        8) echo "eight";;
+        9) echo "nine";;
+        *) echo "default wrong number" ;;
+esac
